80c477f0d728a3d8f5565aba6f6ed9ad61db0a7d
Komal <144211225+KomalkN@users.noreply.github.com>
2024-07-06 11:50:59 +0530

    Update providers.tf

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -1,8 +1,8 @@
 #Provider:----------------------------------
-provider "aws" {
-  access_key = "AKIA3FLDZBWJP3TEXQGR"
-  secret_key = "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"
-}
+#provider "aws" {
+#  access_key = "AKIA3FLDZBWJP3TEXQGR"
+# secret_key = "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"
+#}
 
 terraform {
   backend "s3" {
